f91d45f8ac54b4cfff4a44d5ded5d28844d91d3e
AbhishekSagu <abhishek.sagu@antiersolutions.com>
2021-10-30 18:28:06 +0530

    added banner movie

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "../../axios";
+import requests from "../../Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useState(() => {
+    console.log("**@ inside banner , usestate");
+    const init = async () => {
+      console.log("**@ banner init called");
+      console.log("**@ request url is , ", requests.fetchTrending);
+
+      const response = await axios.get(requests.fetchTrending);
+
+      setMovie(
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length - 1)
+        ]
+      );
+
+      return response;
+    };
+
+    init();
+  }, []);
+
   const truncate = (text, n) => {
     return text?.length > 0 ? text.substring(0, n - 1) + "..." : text;
   };
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png)`,
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
         backgroundPosition: "center center",
       }}
     >
       {/* <h1>https://i.imgur.com/e1hLQ2m.png</h1> */}
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.name || movie?.title || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test description.This is a test description.This is a test
-          description.This is a test description.This is a test description.This
-          is a test description.This is a test description.This is a test
-          description.This is a test description.This is a test description.This
-          is a test description.`,
-            120
-          )}
+          {truncate(movie?.overview, 120)}
         </h1>
       </div>
 
